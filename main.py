d7e3c9f1f5d73e9db53cb76210db148da9953ddf
Andris Jersovs <jersovs.andris@gmail.com>
2023-02-23 16:38:04 +0200

    Added main function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fuelprices.helpers import *
 
-circlek_prices = get_circlek_prices()
-neste_prices = get_neste_prices()
-virsi_prices = get_virsi_prices()
+if __name__ == '__main__':
+    main()
 
-pass
+def main():
+    circlek_prices = get_circlek_prices()
+    neste_prices = get_neste_prices()
+    virsi_prices = get_virsi_prices()
