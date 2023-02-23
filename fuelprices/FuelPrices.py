0eead6628a2ea632cd1034d28d82a549e6d1bd5e
Andris Jersovs <jersovs.andris@gmail.com>
2023-02-23 16:35:08 +0200

    Changed values to float

--- fuelprices/FuelPrices.py
+++ fuelprices/FuelPrices.py
@@ -22,24 +22,24 @@ class FuelPrices:
       fuel_95_price = None
     else:
       fuel_95_price_object = soup.select(fuel_95_css_selector)[0].text
-      fuel_95_price = re.findall(float_regex, fuel_95_price_object)[0]
+      fuel_95_price = float(re.findall(float_regex, fuel_95_price_object)[0])
 
     if fuel_98_css_selector is None:
       fuel_98_price = None
     else:
       fuel_98_price_object = soup.select(fuel_98_css_selector)[0].text
-      fuel_98_price = re.findall(float_regex, fuel_98_price_object)[0]
+      fuel_98_price = float(re.findall(float_regex, fuel_98_price_object)[0])
 
     if fuel_diesel_css_selector is None:
       fuel_diesel_price = None
     else:
       fuel_diesel_price_object = soup.select(fuel_diesel_css_selector)[0].text
-      fuel_diesel_price = re.findall(float_regex, fuel_diesel_price_object)[0]
+      fuel_diesel_price = float(re.findall(float_regex, fuel_diesel_price_object)[0])
 
     if fuel_lpg_css_selector is None:
       fuel_lpg_price = None
     else:
       fuel_lpg_price_object = soup.select(fuel_lpg_css_selector)[0].text
-      fuel_lpg_price = re.findall(float_regex, fuel_lpg_price_object)[0]
+      fuel_lpg_price = float(re.findall(float_regex, fuel_lpg_price_object)[0])
 
     return FuelPrices(station_name, fuel_95_price, fuel_98_price, fuel_diesel_price, fuel_lpg_price)
