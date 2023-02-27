b8fe067321a4ffb4f95d31731b438c44c3304aad
Andris Jersovs <jersovs.andris@gmail.com>
2023-02-27 12:18:32 +0200

    Fixed issues in helper

--- fuelprices/InfluxDbHelper.py
+++ fuelprices/InfluxDbHelper.py
@@ -1,15 +1,14 @@
-import os
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 class InfluxDb:
-    def __init__(self, url, token, org, bucket):
-        self.org = org,
+    def __init__(self, url, token, organization, bucket):
+        self.organization = organization
         self.bucket = bucket
-        client = influxdb_client.InfluxDBClient(url=url, token=token, org=self.org)
+        client = influxdb_client.InfluxDBClient(url=url, token=token, organization=organization)
         self.write_api = client.write_api(write_options=SYNCHRONOUS)
 
     def write_fuel_prices(self, fuel_prices):
         for fuel in fuel_prices.prices:
             record = influxdb_client.Point(fuel).tag("fuel_station", fuel_prices.fuel_station_name).time(fuel_prices.timestamp).field("price", fuel_prices.prices[fuel])
-            self.write_api.write(bucket=self.bucket, org=self.org, record=record)
+            self.write_api.write(bucket=self.bucket, org=self.organization, record=record)
