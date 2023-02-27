0f589ef7910ad3cde62b8746b1b6fcf66335ed31
Andris Jersovs <jersovs.andris@gmail.com>
2023-02-27 13:22:03 +0200

    Added logging

--- fuelprices/GasStations.py
+++ fuelprices/GasStations.py
@@ -1,4 +1,5 @@
 from fuelprices.FuelPrices import FuelPrices
+import logging
 
 class GasStations:
     def get_circlek_prices():
@@ -8,7 +9,7 @@ class GasStations:
         fuel_98_css_selector = '.ck-striped-table > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(2)'
         fuel_diesel_css_selector = '.ck-striped-table > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(2)'
         fuel_lpg_css_selector = '.ck-striped-table > tbody:nth-child(2) > tr:nth-child(4) > td:nth-child(2)'
-        
+        logging.info("Retrieving fuel prices for " + station_name)
         return FuelPrices.get_fuel_prices(station_name, url, fuel_95_css_selector, fuel_98_css_selector, fuel_diesel_css_selector, fuel_lpg_css_selector)
 
     def get_neste_prices():
@@ -18,7 +19,7 @@ class GasStations:
         fuel_98_css_selector = '.field__item > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > p:nth-child(1) > span:nth-child(1) > strong:nth-child(1)'
         fuel_diesel_css_selector = '.field__item > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2) > p:nth-child(1) > span:nth-child(1) > strong:nth-child(1)'
         fuel_lpg_css_selector = None
-        
+        logging.info("Retrieving fuel prices for " + station_name)
         return FuelPrices.get_fuel_prices(station_name, url, fuel_95_css_selector, fuel_98_css_selector, fuel_diesel_css_selector, fuel_lpg_css_selector)
 
     def get_virsi_prices():
@@ -28,5 +29,5 @@ class GasStations:
         fuel_98_css_selector = 'div.prices-block:nth-child(2) > div:nth-child(1) > ul:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > p:nth-child(2) > span:nth-child(2)'
         fuel_diesel_css_selector = 'div.prices-block:nth-child(2) > div:nth-child(1) > ul:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p:nth-child(2) > span:nth-child(2)'
         fuel_lpg_css_selector = 'div.price-card:nth-child(5) > div:nth-child(1) > div:nth-child(1) > p:nth-child(2) > span:nth-child(2)'
-        
+        logging.info("Retrieving fuel prices for " + station_name)
         return FuelPrices.get_fuel_prices(station_name, url, fuel_95_css_selector, fuel_98_css_selector, fuel_diesel_css_selector, fuel_lpg_css_selector)
