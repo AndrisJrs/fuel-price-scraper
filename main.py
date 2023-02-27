0c991049fecd79c10b2b32b8c90fdaa1b1bd588f
Andris Jersovs <jersovs.andris@gmail.com>
2023-02-27 12:03:38 +0200

    Rewritten implementation

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
-import influxdb_client
-from fuelprices.helpers import *
-from influxdb_client.client.write_api import SYNCHRONOUS
+import os
+from fuelprices.GasStations import GasStations
+from fuelprices.InfluxDbHelper import InfluxDb
 
 def main():
-    circlek_prices = get_circlek_prices()
-    neste_prices = get_neste_prices()
-    virsi_prices = get_virsi_prices()
+    circlek_prices = GasStations.get_circlek_prices()
+    neste_prices = GasStations.get_neste_prices()
+    virsi_prices = GasStations.get_virsi_prices()
 
-    bucket = "fuel_prices"
-    org = "primary"
-    token = ""
-    # Store the URL of your InfluxDB instance
-    url="http://localhost:8086"
+    os.environ["INFLUXDB_BUCKET"] = "fuel_prices"
+    os.environ["INFLUXDB_URL"] = "http://localhost:8086"
+    os.environ["INFLUXDB_ORG"] = "primary"
+    os.environ["INFLUXDB_TOKEN"] = ""
 
-    client = influxdb_client.InfluxDBClient(
-        url=url,
-        token=token,
-        org=org
-    )
+    bucket = os.environ['INFLUXDB_BUCKET']
+    url = os.environ['INFLUXDB_URL']
+    org = os.environ['INFLUXDB_ORG']
+    token = os.environ['INFLUXDB_TOKEN']
 
-    write_api = client.write_api(write_options=SYNCHRONOUS)
-
-    record = influxdb_client.Point("fuel_95_price").tag("fuel_station", "pepega").time(circlek_prices.timestamp).field("price", circlek_prices.fuel_95_price)
-    write_api.write(bucket=bucket, org=org, record=record)
+    influxdb = InfluxDb(url, token, org, bucket)
+    influxdb.write_fuel_prices(circlek_prices)
+    influxdb.write_fuel_prices(neste_prices)
+    influxdb.write_fuel_prices(virsi_prices)
 
 if __name__ == '__main__':
     main()
