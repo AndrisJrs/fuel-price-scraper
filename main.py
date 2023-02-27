82fd8205f5cfdb04177bec68e9d0d258666f2eb7
Andris Jersovs <jersovs.andris@gmail.com>
2023-02-27 13:29:47 +0200

    Removed redundant stuff

--- main.py
+++ main.py
@@ -3,19 +3,14 @@ from fuelprices.GasStations import GasStations
 from fuelprices.InfluxDbHelper import InfluxDb
 
 def main():
-    circlek_prices = GasStations.get_circlek_prices()
-    neste_prices = GasStations.get_neste_prices()
-    virsi_prices = GasStations.get_virsi_prices()
-
-    os.environ["INFLUXDB_BUCKET"] = "fuel_prices"
-    os.environ["INFLUXDB_URL"] = "http://localhost:8086"
-    os.environ["INFLUXDB_ORG"] = "primary"
-    os.environ["INFLUXDB_TOKEN"] = ""
-
     bucket = os.environ['INFLUXDB_BUCKET']
     url = os.environ['INFLUXDB_URL']
     org = os.environ['INFLUXDB_ORG']
     token = os.environ['INFLUXDB_TOKEN']
+
+    circlek_prices = GasStations.get_circlek_prices()
+    neste_prices = GasStations.get_neste_prices()
+    virsi_prices = GasStations.get_virsi_prices()
 
     influxdb = InfluxDb(url, token, org, bucket)
     influxdb.write_fuel_prices(circlek_prices)
