d900b01d37bfcde90782f478aaff107d9bcda8ee
Andris Jersovs <jersovs.andris@gmail.com>
2023-02-27 12:03:38 +0200

    Rewritten implementation

---
+++ fuelprices/InfluxDbHelper.py
@@ -0,0 +1,15 @@
+import os
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+class InfluxDb:
+    def __init__(self, url, token, org, bucket):
+        self.org = org,
+        self.bucket = bucket
+        client = influxdb_client.InfluxDBClient(url=url, token=token, org=self.org)
+        self.write_api = client.write_api(write_options=SYNCHRONOUS)
+
+    def write_fuel_prices(self, fuel_prices):
+        for fuel in fuel_prices.prices:
+            record = influxdb_client.Point(fuel).tag("fuel_station", fuel_prices.fuel_station_name).time(fuel_prices.timestamp).field("price", fuel_prices.prices[fuel])
+            self.write_api.write(bucket=self.bucket, org=self.org, record=record)
