3e927b5ebca1f5b36d14fe203cb0be48b6585cfc
Andris Jersovs <jersovs.andris@gmail.com>
2023-11-22 19:47:14 +0200

    Fixed neste selectors and added exception handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from fuelprices.GasStations import GasStations
 from fuelprices.InfluxDbHelper import InfluxDb
 
@@ -8,16 +9,35 @@ def main():
     org = os.environ['INFLUXDB_ORG']
     token = os.environ['INFLUXDB_TOKEN']
 
-    circlek_prices = GasStations.get_circlek_prices()
-    neste_prices = GasStations.get_neste_prices()
-    virsi_prices = GasStations.get_virsi_prices()
-    viada_prices = GasStations.get_viada_prices()
-
     influxdb = InfluxDb(url, token, org, bucket)
-    influxdb.write_fuel_prices(circlek_prices)
-    influxdb.write_fuel_prices(neste_prices)
-    influxdb.write_fuel_prices(virsi_prices)
-    influxdb.write_fuel_prices(viada_prices)
+
+    try:
+        circlek_prices = GasStations.get_circlek_prices()
+        influxdb.write_fuel_prices(circlek_prices)
+    except Exception as e:
+        logging.error("Failed to retrieve CircleK prices")
+        logging.exception(e)
+
+    try:
+        neste_prices = GasStations.get_neste_prices()
+        influxdb.write_fuel_prices(neste_prices)
+    except Exception as e:
+        logging.error("Failed to retrieve Neste prices")
+        logging.exception(e)
+
+    try:
+        virsi_prices = GasStations.get_virsi_prices()
+        influxdb.write_fuel_prices(virsi_prices)
+    except Exception as e:
+        logging.error("Failed to retrieve Virsi prices")
+        logging.exception(e)
+
+    try:
+        viada_prices = GasStations.get_viada_prices()
+        influxdb.write_fuel_prices(viada_prices)
+    except Exception as e:
+        logging.error("Failed to retrieve Viada prices")
+        logging.exception(e)
 
 if __name__ == '__main__':
     main()
