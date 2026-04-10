4fbaa368b78d6ddf5811490f8c91da93551c4c7b
Andris Jersovs <andris.jersovs@protonmail.com>
2026-04-10 23:59:17 +0300

    Verify false for viada

--- fuelprices/GasStations.py
+++ fuelprices/GasStations.py
@@ -128,7 +128,7 @@ class GasStations:
         logging.info("Retrieving fuel prices for " + station_name)
 
         logging.debug("Getting URL " + url)
-        response = requests.get(url)
+        response = requests.get(url, verify=False)
 
         fuel_prices = []
 
