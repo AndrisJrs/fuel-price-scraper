0f236773858a65a4ae69eb0fe2c564716609cb63
Andris Jersovs <andris.jersovs@protonmail.com>
2026-04-29 11:20:47 +0300

    Fixed selector

--- fuelprices/GasStations.py
+++ fuelprices/GasStations.py
@@ -59,7 +59,7 @@ class GasStations:
         station_name = "Neste"
         url = "https://www.neste.lv/lv/content/degvielas-cenas"
         fuel_95_price_css_selector = '.field__item > table:nth-child(4) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > p:nth-child(1) > span:nth-child(1) > span:nth-child(1) > strong:nth-child(1)'
-        fuel_95_location_css_selector = '.field__item > table:nth-child(4) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(3) > p:nth-child(2)'
+        fuel_95_location_css_selector = '.field__item > table:nth-child(4) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(3) > p:nth-child(1)'
         fuel_98_price_css_selector = '.field__item > table:nth-child(4) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > p:nth-child(1) > span:nth-child(1) > span:nth-child(1) > strong:nth-child(1)'
         fuel_98_location_css_selector = '.field__item > table:nth-child(4) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(3)'
         fuel_diesel_price_css_selector = '.field__item > table:nth-child(4) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2) > p:nth-child(1) > font:nth-child(1) > span:nth-child(1) > b:nth-child(1)'
